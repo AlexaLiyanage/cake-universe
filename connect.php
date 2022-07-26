3bd07c9c506ab425383a1291d31c34daf4926eb4
Ama Liyanage <109949511+AlexaLiyanage@users.noreply.github.com>
2022-07-26 12:00:28 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $c_name = $_POST['c_name'];
+    $address = $_POST['address'];
+
+    var_dump($name, $email, $number, $c_name, $address);
+
+    //database connection
+
+?>
