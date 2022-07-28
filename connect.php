f24b614e9d549c93ea5dc4d2bee983d6b9f4b8e7
Ama Liyanage <109949511+AlexaLiyanage@users.noreply.github.com>
2022-07-28 20:43:59 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,8 +5,21 @@
     $c_name = $_POST['c_name'];
     $address = $_POST['address'];
 
-    var_dump($name, $email, $number, $c_name, $address);
-
+   
     //database connection
+
+    $conn = new mysqli('localhost','root','','test');
+    if($conn->connect_error){
+        die('Connection Failed :' .$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into cakeorder(name, email, number, c_name, address)
+        values(?, ?, ?, ?, ?)");
+        $stmt-> bind_param("ssiss", $name, $email, $number, $c_name, $address);
+        $stmt->execute();
+        echo "Order Successfully......";
+        $stmt->close();
+        $conn->close();
+    }
+
 
 ?>
